67e3abf4d9c174fca19adf530fc70f55ba03cd46
Maxim <maxim25072003@gmail.com>
2022-03-17 13:58:20 +0700

    beta A*

--- js/maze.js
+++ js/maze.js
@@ -7,75 +7,251 @@ const BACKGROUND_COLOR = "gray";
 const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
 
-document.getElementById("mybutton").onclick = click;
+document.getElementById("mybutton").onclick = generateMap;
 
-function click(){
-    //get input
-    let a = Number(document.getElementById("myinput").value);
-    console.log(a);
-    const COLUMNS = a;
-    const ROWS = a;   
-    // canvas.width = PAD * 2 + COLUMNS * SIZE;
-    // canvas.height = PAD * 2 + ROWS  * SIZE;
-    context.clearRect(0, 0, 500, 500)
+let sizeNode;
+let matrix;
+let n;
+let beginCell = [-1, -1]
+let endCell = [-1, -1]
+let typeOfClick = 0; // 0 - place walls, 1 - begin cell, 2 - end cell
+
+document.getElementById("alg").onclick = function() { A_star(beginCell, endCell) };
+
+function generateMap(){
+    n = Number(document.getElementById("myinput").value);//get input
+
+    console.log(n);
+    context.clearRect(0, 0, 500, 500);
     context.beginPath();
+    sizeNode = Math.floor(500/n);
+    matrix = getMatrix(n);
 
-    const sizeNode = Math.floor(500/a);
+    console.log("-->  " + sizeNode + "  " + Number.isInteger(sizeNode));
 
-    // if (!Number.isInteger(sizeNode)){
-    //     canvas.width = Math.round(sizeNode * a) + 5
-    //     canvas.height = Math.round(sizeNode * a) + 5
-    // }
-
-    console.log("-->  " + sizeNode + "  " + Number.isInteger(sizeNode))
-
-    for (let x = 0; x <= 500; x += sizeNode) {
+    let x = 0;
+    let y = 0;
+    for (let i = 0; i <= n; i++) {
         context.moveTo(x, 0);
-        context.lineTo(x, sizeNode * a);
+        context.lineTo(x, sizeNode * n);
+        x += sizeNode;
     }
 
-    for (let y = 0; y <= 500; y += sizeNode) {
+    for (let i = 0; i <= n; i++) {
         context.moveTo(0, y);
-        context.lineTo(sizeNode * a, y);
+        context.lineTo(sizeNode * n, y);
+        y += sizeNode;
     }
 
-    //context.strokeStyle = "#888";
     context.stroke();
-
-    /*
-    context.beginPath();
-    context.rect(0, 0, canvas.width, canvas.height);
-    context.fillStyle = BACKGROUND_COLOR;
-    context.fill();*/
-
+    canvas.addEventListener('click', mouseClick);
+    document.getElementById("beginCell").onclick = btn1;
+    document.getElementById("endCell").onclick = btn2;
+    document.getElementById("walls").onclick = btn3;
+}
+function btn1(){
+    typeOfClick = 1;
+}
+function btn2(){
+    typeOfClick = 2;
+}
+function btn3(){
+    typeOfClick = 0;
 }
 
-const matrix = getMatrix(COLUMNS, ROWS);
-const eraser = {
-    x: 0, y: 0,
-};
-
-function getMatrix(columns, rows) {
-    const matrix = [];
-
-    for (let y = 0; y < rows; y++) {
-        const row = [];
-        for (let x = 0; x < columns; x++) {
-            row.push(false);
+function mouseClick(e){
+    let clientX = e.pageX - e.target.offsetLeft;
+    let clientY = e.pageY - e.target.offsetTop;
+    let i = Math.floor(clientX / sizeNode);
+    let j = Math.floor(clientY / sizeNode);
+    console.log(`x:${clientX}, y:${clientY}`);
+    let cellX = Math.floor(clientX / sizeNode) * sizeNode;
+    let cellY = Math.floor(clientY / sizeNode) * sizeNode;
+    if (typeOfClick === 0){
+        if (matrix[i][j]){
+            matrix[i][j] = 0;
+            context.fillStyle="#ffffff";
+            context.fillRect(cellX + 0.5, cellY + 0.5, sizeNode - 1, sizeNode - 1);
         }
-        matrix.push(row);
+        else {
+            matrix[i][j] = 1;
+            context.fillStyle="#000000";
+            context.fillRect(cellX, cellY, sizeNode, sizeNode);
+        }
+    }
+    if (typeOfClick === 1){
+        if (JSON.stringify(beginCell) !== JSON.stringify([-1, -1])){
+            context.fillStyle="#ffffff";
+            context.fillRect(beginCell[0] * sizeNode + 0.5, beginCell[1] * sizeNode + 0.5, sizeNode - 1, sizeNode - 1);
+        }
+        context.fillStyle="#0cfa00";
+        context.fillRect(cellX + 0.5, cellY + 0.5, sizeNode - 1, sizeNode - 1);
+        beginCell[0] = i;
+        beginCell[1] = j;
+    }
+
+    if (typeOfClick === 2){
+        if (JSON.stringify(endCell) !== JSON.stringify([-1, -1])){
+            context.fillStyle="#ffffff";
+            context.fillRect(endCell[0] * sizeNode + 0.5, endCell[1] * sizeNode + 0.5, sizeNode - 1, sizeNode - 1);
+        }
+        context.fillStyle="#ff0216";
+        context.fillRect(cellX + 0.5, cellY + 0.5, sizeNode - 1, sizeNode - 1);
+        endCell[0] = i;
+        endCell[1] = j;
+    }
+}
+
+function getMatrix(a) {
+    let matrix = new Array(a);
+    for (let i = 0; i < a; i++) {
+        matrix[i] = new Array(a);
+    }
+    for (let i = 0; i < a; ++i){
+        for (let j = 0; j < a; ++j){
+            matrix[i][j] = 0;
+        }
     }
     return matrix;
 }
 
-/*function drawMaze() {
-    canvas.width = PAD * 2 + COLUMNS * SIZE;
-    canvas.height = PAD * 2 + ROWS  * SIZE;
+function PriorityQueue() { // input: [[x, y], priority]
+    let array = [];
 
-    context.beginPath();
-    context.rect(0, 0, canvas.width, canvas.height);
-    context.fillStyle = BACGROUND_COLOR;
-    context.fill();
+    this.print = function() {
+        console.log(array);
+    }
+
+    this.enqueue = function(element) {
+        if (this.isEmpty()) {
+            array.push(element);
+        } else {
+            let added = false
+            for (let i = 0; i < array.length; i++) {
+                if (element[1] < array[i][1]) {
+                    console.log(array);
+                    array.splice(i, 0, element);
+                    console.log(array);
+                    added = true;
+                    break;
+                }
+            }
+            if (!added) {
+                array.push(element);
+            }
+        }
+    }
+
+    this.dequeue = function() {
+        return array.shift();
+    }
+
+    this.front = function() {
+        return array[0];
+    }
+
+    this.isEmpty = function() {
+        return array.length === 0;
+    }
+
+    this.size = function() {
+        return array.length;
+    }
+}
+
+function heuristicFunc(current, end){
+    return Math.abs(current[0] - end[0]) + Math.abs(current[1] - end[1]);
+}
+
+function unVisitedNeighbours(current, visited){
+    let neighbours = []
+    let curX = current[0];
+    let curY = current[1];
+    if (curX + 1 < n && !visited[curX + 1][curY] && !matrix[curX + 1][curY]){
+        neighbours.push([curX + 1, curY]);
+    }
+    if (curY + 1 < n && !visited[curX][curY + 1] && !matrix[curX][curY + 1]){
+        neighbours.push([curX, curY + 1]);
+    }
+    if (curX - 1 >= 0 && !visited[curX - 1][curY] && !matrix[curX - 1][curY]){
+        neighbours.push([curX - 1, curY]);
+    }
+    if (curY - 1 >= 0 && !visited[curX][curY - 1]&& !matrix[curX][curY - 1]){
+        neighbours.push([curX, curY - 1]);
+    }
+    return neighbours
+}
+
+function A_star(start, end){
+    if (JSON.stringify(start) === JSON.stringify([-1, -1])){
+        alert("Установите начальную клетку!");
+        return;
+    }
+
+    if (JSON.stringify(end) === JSON.stringify([-1, -1])){
+        alert("Установите конечную клетку!");
+        return;
+    }
+
+    let queue = new PriorityQueue();
+    let visited = getMatrix(n);
+    let costTo = getMatrix(n);
+
+    let parent = new Array(n);
+    for (let i = 0; i < n; i++) {
+        parent[i] = new Array(n);
+    }
+    for (let i = 0; i < n; ++i){
+        for (let j = 0; j < n; ++j){
+            parent[i][j] = [-1, -1];
+        }
+    }
+    queue.enqueue([[start[0], start[1]], heuristicFunc(start, end)])
+    visited[start[0]][start[1]] = 1;
+
+    while (!queue.isEmpty()){
+        let current = queue.dequeue()
+        let curX = current[0][0];
+        let curY = current[0][1];
+        if (JSON.stringify(current[0]) === JSON.stringify(end)){
+            break;
+        }
+
+        let neighbours = unVisitedNeighbours([curX, curY], visited);
+        for (let i = 0; i < neighbours.length; ++i){
+            let currentNeighbour = neighbours[i];
+            let currentNeighbourX = neighbours[i][0];
+            let currentNeighbourY = neighbours[i][1];
+            context.fillStyle = "#FFC567FF";
+            context.fillRect(currentNeighbourX * sizeNode + 0.5, currentNeighbourY * sizeNode + 0.5, sizeNode - 1, sizeNode - 1);
+
+            if (!visited[currentNeighbourX][currentNeighbourY] || costTo[curX][curY] + 1 < costTo[currentNeighbourX][currentNeighbourY]){
+                parent[currentNeighbourX][currentNeighbourY] = [curX, curY];
+                costTo[currentNeighbourX][currentNeighbourY] = costTo[curX][curY] + 1;
+            }
+            if (!visited[currentNeighbourX][currentNeighbourY]){
+                queue.enqueue([currentNeighbour, costTo[currentNeighbourX][currentNeighbourY] + heuristicFunc(currentNeighbour, end)])
+                visited[currentNeighbourX][currentNeighbourY] = 1;
+            }
+        }
+
+
+    }
+
+    if (JSON.stringify(parent[end[0]][end[1]]) !== JSON.stringify([-1, -1])){
+        let current = end;
+        while (JSON.stringify(current) !== JSON.stringify([-1, -1])){
+            context.fillStyle = "#06d9fd";
+            context.fillRect(current[0] * sizeNode + 0.5, current[1] * sizeNode + 0.5, sizeNode - 1, sizeNode - 1);
+            current = parent[current[0]][current[1]];
+        }
+        context.fillStyle="#ff0216";
+        context.fillRect(end[0] * sizeNode + 0.5, end[1] * sizeNode + 0.5, sizeNode - 1, sizeNode - 1);
+        context.fillStyle="#0cfa00";
+        context.fillRect(start[0] * sizeNode + 0.5, start[1] * sizeNode + 0.5, sizeNode - 1, sizeNode - 1);
+    }
+    else{
+        alert("НЕТ ПУТИ :(")
+    }
 
 }
-*/
