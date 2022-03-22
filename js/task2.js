643b4222f841699d77a9d228d720ca4d3aa7a321
devloliconic <kirillevsmirnov@gmail.com>
2022-03-22 21:22:35 +0700

    cluster beta version

--- js/task2.js
+++ js/task2.js
@@ -4,18 +4,22 @@ let coardDot = [];
 let greenBox = [];
 let redBox = [];
 let centroidarry = [];
-
+// let coardArrGreen =[];
+// let coardArrRed = [];
+let coardAll = [];
+var floagForPoint = true;
 
 canvas.onclick = function (event) {
-    var x = event.offsetX;
-    var y = event.offsetY;
-    coardDot.push({x,y});
-    ctx.fillStyle = "black";
-    ctx.beginPath();
-    ctx.arc(x,y,10,0,2*Math.PI);
-    ctx.stroke();
-    ctx.fill();
-    
+    if (floagForPoint === true){
+        var x = event.offsetX;
+        var y = event.offsetY;
+        coardDot.push({x,y});
+        ctx.fillStyle = "black";
+        ctx.beginPath();
+        ctx.arc(x,y,10,0,2*Math.PI);
+        ctx.stroke();
+        ctx.fill();
+    }
   return coardDot
 }
 
@@ -45,14 +49,15 @@ function addCentroid(){
         ctx.fill();
     }
     flagLimitedCentroid = false;
-    return centroidarry
+    floagForPoint = false;
+    return centroidarry;
 }
-console.log(coardDot)
-console.log(centroidarry);
+// console.log(coardDot)
+// console.log(centroidarry);
 document.getElementById("startButton").onclick = buildCluster;
 function buildCluster(){ 
     for(let coard of coardDot){
-        console.log(coard.x)
+        // console.log(coard.x);
         let chekGreenX = coard.x - centroidarry[0];
         let chekGreenY = coard.y - centroidarry[1];
         let chekRedX = coard.x- centroidarry[2];
@@ -68,8 +73,6 @@ function buildCluster(){
             redBox.push(coard);
         }
     }
-    console.log(greenBox);
-    console.log(redBox);
     for(let i = 0; i < greenBox.length;i++){
         ctx.fillStyle = "green";
         ctx.beginPath();
@@ -85,3 +88,54 @@ function buildCluster(){
         ctx.fill();
     }
 }
+document.getElementById("nextStep").onclick = avgCoard;
+console.log(greenBox);
+console.log(redBox);
+console.log(centroidarry);
+function avgCoard(){
+    let sumX = 0;
+    let sumY = 0;
+    let sredX = 0;
+    let sredY = 0;
+    for(let i = 0; i < greenBox.length; i++){
+        sumX = greenBox[i].x + sumX;
+    }
+    sredX = sumX/greenBox.length;
+    coardAll.push(sredX);
+    for(let i = 0; i < greenBox.length; i++){
+        sumY = greenBox[i].y + sumY;
+    }
+    sredY = sumY/greenBox.length;
+    coardAll.push(sredY);
+    sumY = 0;
+    sumX = 0;
+    for(let i = 0; i < redBox.length; i++){
+        sumX = redBox[i].x + sumX;
+    }
+    sredX = sumX/redBox.length;
+    coardAll.push(sredX);
+    for(let i = 0; i < redBox.length; i++){
+        sumY = redBox[i].y + sumY;
+    }
+    sredY = sumY/redBox.length;
+    coardAll.push(sredY);
+    ctx.clearRect(centroidarry[0], centroidarry[1], 500, 500);
+    ctx.clearRect(centroidarry[2], centroidarry[3], 500, 500);
+    centroidarry[0] = coardAll[0];
+    centroidarry[1] = coardAll[1];
+    centroidarry[2] = coardAll[2];
+    centroidarry[3] = coardAll[3];
+    ctx.fillStyle = "green";
+    ctx.beginPath();
+    ctx.fillRect(centroidarry[0], centroidarry[1], 20, 20);
+    ctx.fill();
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.fillRect(centroidarry[2], centroidarry[3], 20, 20);
+    ctx.fill();
+    greenBox = [];
+    redBox = [];
+    console.log(centroidarry);
+    buildCluster();
+} 
+console.log(centroidarry);
